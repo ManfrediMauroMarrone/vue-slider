1c4007a406d1a1bd9c8b393dbe10d686f7b7764a
Manfredi Mauro Marrone <manfredi.marrone@hotmail.it>
2020-11-09 15:12:15 +0100

    creato slider

---
+++ main.js
@@ -0,0 +1,11 @@
+var app = new Vue({
+  el: '#root',
+
+  data: {
+    images: [
+      'https://www.muralswallpaper.com/app/uploads/Green-Tropical-Plant-Wallpaper-Mural-Plain.jpg',
+      'https://steamuserimages-a.akamaihd.net/ugc/940586530515504757/CDDE77CB810474E1C07B945E40AE4713141AFD76/',
+      'https://i.imgur.com/sjvtlq0.jpg',
+      'https://images6.alphacoders.com/859/thumb-1920-859553.png'
+  }
+})
